cb7e80d680a62692d43b46e613a24496fb42806a
Voß, Arne <arne.voss@dlr.de>
2022-05-10 16:25:40 +0200

    Adding some basic test cases using the simplewing

---
+++ tests/test_integration.py
@@ -0,0 +1,68 @@
+import sys, pickle
+
+"""
+This section sets the environment for the gitlab-runner, which uses the functional account 'f_jwsb'.
+First, we  add the location of the Panel Aero to the python path.
+"""
+sys.path.append("../panel-aero")
+from panelaero import VLM, DLM
+from helper_functions import HelperFunctions
+
+class TestSimplewing(HelperFunctions):
+    # Load geometry 
+    with open('./tests/reference_data/simplewing_aerogrid.pickle', 'rb') as fid:
+        aerogrid = pickle.load(fid)
+ 
+    def test_VLM_Ma00(self):      
+        # Calculate the AIC matrix
+        Qjj = VLM.calc_Qjj(self.aerogrid, Ma=0.0) 
+        # Do the comparison
+        print('Comparing AIC with reference')
+        with open('./tests/reference_data/simplewing_VLM_Ma00.pickle', 'rb') as fid:
+            reference_data = pickle.load(fid)
+        assert self.compare_AICs(Qjj, reference_data, self.aerogrid['n']), "AIC does NOT match reference"
+        
+    def test_VLM_Ma03(self):      
+        # Calculate the AIC matrix
+        Qjj = VLM.calc_Qjj(self.aerogrid, Ma=0.3) 
+        # Do the comparison
+        print('Comparing AIC with reference')
+        with open('./tests/reference_data/simplewing_VLM_Ma03.pickle', 'rb') as fid:
+            reference_data = pickle.load(fid)
+        assert self.compare_AICs(Qjj, reference_data, self.aerogrid['n']), "AIC does NOT match reference"
+ 
+    def test_DLM_Ma00_k02(self):      
+        # Calculate the AIC matrix
+        Qjj = DLM.calc_Qjj(self.aerogrid, Ma=0.0, k=0.2) 
+        # Do the comparison
+        print('Comparing AIC with reference')
+        with open('./tests/reference_data/simplewing_DLM_Ma00_k02.pickle', 'rb') as fid:
+            reference_data = pickle.load(fid)
+        assert self.compare_AICs(Qjj, reference_data, self.aerogrid['n']), "AIC does NOT match reference"
+    
+    def test_DLM_Ma03_k02(self):      
+        # Calculate the AIC matrix
+        Qjj = DLM.calc_Qjj(self.aerogrid, Ma=0.3, k=0.2) 
+        # Do the comparison
+        print('Comparing AIC with reference')
+        with open('./tests/reference_data/simplewing_DLM_Ma03_k02.pickle', 'rb') as fid:
+            reference_data = pickle.load(fid)
+        assert self.compare_AICs(Qjj, reference_data, self.aerogrid['n']), "AIC does NOT match reference"
+
+    def test_DLM_Ma00_k02_quartic(self):      
+        # Calculate the AIC matrix
+        Qjj = DLM.calc_Qjj(self.aerogrid, Ma=0.0, k=0.2, method='quartic') 
+        # Do the comparison
+        print('Comparing AIC with reference')
+        with open('./tests/reference_data/simplewing_DLM_Ma00_k02_quartic.pickle', 'rb') as fid:
+            reference_data = pickle.load(fid)
+        assert self.compare_AICs(Qjj, reference_data, self.aerogrid['n']), "AIC does NOT match reference"
+    
+    def test_DLM_Ma03_k02_quartic(self):      
+        # Calculate the AIC matrix
+        Qjj = DLM.calc_Qjj(self.aerogrid, Ma=0.3, k=0.2, method='quartic') 
+        # Do the comparison
+        print('Comparing AIC with reference')
+        with open('./tests/reference_data/simplewing_DLM_Ma03_k02_quartic.pickle', 'rb') as fid:
+            reference_data = pickle.load(fid)
+        assert self.compare_AICs(Qjj, reference_data, self.aerogrid['n']), "AIC does NOT match reference"
