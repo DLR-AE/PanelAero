26f0bbd7e1bcf3b9d21af52ef7cbb07e3407987b
Arne Voß <arne.voss@dlr.de>
2023-04-28 18:47:36 +0200

    Update version number

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# Release 2023.04
+- Update of the documentation to Version 1.04
+- Added warning for upside down / flipped panels
+
 # Release 2023.02
 - Publication as Open Source under the BSD 3-Clause License 
 - No technical changes, only of administrative nature and with respect to the move to GitHub
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 def my_setup():
     setup(name='Panel-Aero',
-          version='2023.02',
+          version='2023.04',
           description='An Implementation of the Vortex Lattice and the Doublet Lattice Method.',
           url='https://wiki.dlr.de/display/AE/An+Implementation+of+the+Vortex+Lattice+and+the+Doublet+Lattice+Method',
           author='Arne Voß',
