f6404b5e45be517d4a978240c8d84901302ad65f
Arne Voß <arne.voss@dlr.de>
2024-01-23 14:05:40 +0100

    - Add Jupyter-book to regression testing - Add dependencies for [tutorials] to setup.py

--- setup.py
+++ setup.py
@@ -19,10 +19,12 @@ def my_setup():
           install_requires=['numpy'],
           extras_require={'test': ['pytest',
                                    'pytest-cov',
-                                   'jupyter',
-                                   'jupyter-book',
-                                   'mayavi',
-                                   ]},
+                                   ],
+                          'tutorials': ['jupyter',
+                                        'jupyter-book',
+                                        'mayavi',
+                                        ]
+                          },
           )
 
 
