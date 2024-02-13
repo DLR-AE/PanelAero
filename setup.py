277670a396d92ce705d6f4b421931f148c5795e1
Arne Voß <arne.voss@dlr.de>
2024-02-13 17:54:15 +0100

    Use utf8 encoding

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def my_setup():
     setup(name='PanelAero',
           version='2024.01',
           description='An Implementation of the Vortex Lattice and the Doublet Lattice Method.',
-          long_description=open('README.md').read(),
+          long_description=open('README.md', encoding='utf8').read(),
           long_description_content_type='text/markdown',
           url='https://github.com/DLR-AE/PanelAero',
           author='Arne Voß',
